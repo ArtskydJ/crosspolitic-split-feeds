4f92d5779150670340dee748897e7b2580075cbb
ArtskydJ <1833684+ArtskydJ@users.noreply.github.com>
2023-03-04 23:21:42 -0600

    Dynamically split feeds from their new naming scheme

--- src/crosspolitic-feed-splitter.js
+++ src/crosspolitic-feed-splitter.js
@@ -2,7 +2,14 @@ const fs = require(`fs`)
 const path = require(`path`)
 
 const split_rss = require(`./split-rss.js`)
-const new_feed_definitions = require('./new-feed-definitions.js')
+
+const title_to_rss_name = {
+	'The Pugcast': `theologypugcast`,
+	'AD on FLF Network': `adrobles`,
+	'The Patriarchy Podcast': `patriachy`,
+	'Daily News Brief': `dailynews`,
+	'The Doane Cast': `darrendoane`,
+}
 
 const fetch2 = (...args) => {
 	return fetch(...args)
@@ -17,7 +24,6 @@ const fetch2 = (...args) => {
 const resolve = filename => path.join(__dirname, filename)
 const read_file = filename => fs.readFileSync(resolve(filename), { encoding: `utf-8` })
 const write_file = (filename, text) => fs.writeFileSync(resolve(filename), text, { encoding: `utf-8` })
-const write_file_json = (filename, obj) => write_file(resolve(filename), JSON.stringify(obj, null, `\t`))
 
 const get_rss_url = rss_name => `https://artskydj.github.io/crosspolitic-split-feeds/rss/${ rss_name }`
 
@@ -26,35 +32,38 @@ const main = async() => {
 	write_file(`../tmp/crosspolitic.rss`, body)
 	// const body = read_file(`../tmp/crosspolitic.rss`)
 
-	const all_new_feed_definitions = [
-		...new_feed_definitions.matches,
-		new_feed_definitions.default,
-	]
-
 	const { begin_channel, items, end_channel } = split_rss(body)
 
 	const bins = {}
-	all_new_feed_definitions.forEach(({ rss_name }) => bins[rss_name] = [])
 
 	items.forEach(rss_item => {
-		const [ , item_title ] = rss_item.match(/<title>(.+?)<\/title>/)
-		const { rss_name } =
-			new_feed_definitions.matches.find(({ regex }) => regex.test(item_title)) ||
-			new_feed_definitions.default
-		bins[rss_name].push(rss_item)
+		const title = rss_item.match(/<title>(?:<!\[CDATA\[)?.+\[(.+?)\](?:\]\]>)?<\/title>/)?.[1]
+		const rss_name = (
+			title
+				? title_to_rss_name[title] || title.replaceAll(/\W+/g, '_').toLowerCase()
+				: `everythingelse`
+		) + `.rss`
+
+		if (!bins[rss_name]) {
+			bins[rss_name] = {
+				title,
+				items: []
+			}
+		}
+		bins[rss_name].items.push(rss_item)
 	})
 
-	all_new_feed_definitions.forEach(({ rss_name, title }) => {
+	Object.entries(bins).forEach(([ rss_name, { title, items }]) => {
 		const new_begin_channel = begin_channel
 			.replace(/<title>.+?<\/title>/, `<title>FLF - ${ title }</title>`)
 			.replace(/<atom:link href=".+?"/, `<atom:link href="${ get_rss_url(rss_name) }"`)
-		const new_feed_items = bins[rss_name]
-		const new_feed = [ new_begin_channel, ...new_feed_items, end_channel ].join(``)
+		const new_feed = [ new_begin_channel, ...items, end_channel ].join(``)
 		write_file(`../rss/${ rss_name }`, new_feed)
 	})
 
-	const markdown_list = all_new_feed_definitions
-		.map(({ rss_name, title }) => `- [${ title }](${ get_rss_url(rss_name) }) (${bins[rss_name].length} episodes)`)
+	const markdown_list = Object.entries(bins)
+		.sort((a, b) => a[1].title > b[1].title ? 1 : a[1].title < b[1].title ? -1 : 0)
+		.map(([ rss_name, { title, items }]) => `- [${ title }](${ get_rss_url(rss_name) }) (${ items.length } episodes)`)
 		.join(`\n`)
 	const current_readme = read_file(`../README.md`)
 	const new_readme = current_readme
